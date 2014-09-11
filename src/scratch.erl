b095b82f81eaf8db6426c9788f04d081564ea671
Mike Hurd <leahcim77@gmail.com>
2014-09-11 21:22:52 +0100

    Added map (and reverse)

--- src/scratch.erl
+++ src/scratch.erl
@@ -10,8 +10,16 @@
 -author("mhurd").
 
 %% API
--export([length/1]).
+-export([length/1, map/2]).
 
 length(List) -> length(List, 0).
 length([], Acc) -> Acc;
 length([_|T], Acc) -> length(T, Acc+1).
+
+reverse(List) -> reverse(List, []).
+reverse([], Acc) -> Acc;
+reverse([H|T], Acc) -> reverse(T, [H|Acc]).
+
+map(F, List) -> map(F, List, []).
+map(_, [], Acc) -> reverse(Acc);
+map(F, [H|T], Acc) -> map(F, T, [F(H)|Acc]).
